9c6d1a5b4cab58bf9accebe8d296300154ff1c5e
EvanHerman <evanmherman@gmail.com>
2013-10-25 12:14:17 -0400

    create initial release

--- wp-svg-icon-set1-plugin.php
+++ wp-svg-icon-set1-plugin.php
@@ -1,47 +1,51 @@
 <?php
 /*
-Plugin Name: WordPress Icons - SVG
+Plugin Name: WordPress Icons (SVG)
 Plugin URI: http://evan-herman.com/wp-svg-icon-set-1-example/
 Description: Easily insert svg icons directly in to your WordPress blog with this plugin.
-Version: 1.3.1
 Author: Evan Herman
+Version: 1.0
 Author URI: http://www.Evan-Herman.com
-License:
-	Copyright 2013  Evan Herman (email : Evan.M.Herman@gmail.com)
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+License: GPL2
 */
 
 /***********************************
 
+
+
 *	INCLUDES
+
+
 
 ***********************************/
 
+
+
+
+
+
+
 /* path to any included files */
+
+
+
 include(dirname(__FILE__) . '/includes/scripts.php'); // this controls all js/css
+
 
 // create menu page to display icons
 //Create Menu Page
-function wordpress_svg_icons_plugin_add_menu_page(){
+
+function svg_icon_set1_plugin_add_menu_page(){
 
 	 add_menu_page( 'WP Icon Set#1', 
 
-						 'SVG Icons Free', 
+						 'WP Icon Set#1', 
 
 						 'manage_options', 
 
-						 'wordpress_svg_icons_cheat_sheet', 
+						 'svg_icon_set1_cheat_sheet', 
 
-						 'wordpress_svg_icons_cheat_sheet',
+						 'svg_icon_set1_cheat_sheet',
 
 						 plugins_url( '/svg-vector-icon-plugin/includes/images/svg-set1-menu-icon.png' )
 
@@ -49,15 +53,29 @@ function wordpress_svg_icons_plugin_add_menu_page(){
 
 }
 
-add_action('admin_menu', 'wordpress_svg_icons_plugin_add_menu_page');
+add_action('admin_menu', 'svg_icon_set1_plugin_add_menu_page');
 
-function wordpress_svg_icons_cheat_sheet(){
+function svg_icon_set1_cheat_sheet(){
 ?>
 
 <html>
 <head>
-<link rel="stylesheet" href="<?php echo plugins_url( '/svg-vector-icon-plugin/includes/css/wordpress-svg-icon-plugin-style.css' );?>" />
-<script src="<?php echo plugins_url('/svg-vector-icon-plugin/includes/js/wordpress-svg-icon-plugin-scripts.js');?>"></script>
+<link rel="stylesheet" href="<?php echo plugins_url( '/svg-vector-icon-plugin/includes/css/wp-svg-icon-set1-plugin-style.css' );?>" />
+<script>
+jQuery(document).ready(function(){
+	jQuery('input[type=text].glyph_unicode').click(buttonClick);
+	
+	function buttonClick() {		
+		var glyphUnicode =  jQuery(this).val();
+				jQuery('input[type=text].copy_paste_input').val('<div data-icon="'+ glyphUnicode + '"></div>');
+				jQuery('input[type=text].glyph_unicode').css('background-color','#eee');
+				jQuery(this).css('background-color','#FF8000', 'font-color', '#000');
+				
+				jQuery('.copy-paste-text').fadeIn('slow');
+				jQuery('.wp-svg-icon-preview').html('<div class="wp-svg-iconset1-preview" data-icon="'+ glyphUnicode + '"></div>');
+			};		
+});
+</script>
 <style>
 	section, header, footer {display: block;}
 	body {
@@ -73,7 +91,7 @@ function wordpress_svg_icons_cheat_sheet(){
 		margin: 0;
 		padding: 0;
 	}
-	.glyph, .expansion-Glyph {
+	.glyph {
 		font-size: 16px;
 		float: left;
 		text-align: center;
@@ -84,7 +102,7 @@ function wordpress_svg_icons_cheat_sheet(){
 		border-radius: .25em;
 		box-shadow: inset 0 0 0 1px #f8f8f8, 0 0 0 1px #CCC;
 	}
-	.glyph input, .expansion-Glyph input {
+	.glyph input {
 		font-family: consolas, monospace;
 		font-size: 13px;
 		width: 100%;
@@ -93,7 +111,6 @@ function wordpress_svg_icons_cheat_sheet(){
 		box-shadow: 0 0 0 1px #ccc;
 		padding: .125em;
 	}
-	.expansion-Glyph:hover { background-color: #CCCCCC !important; cursor:pointer !important; }
 	.w-main {
 		width: 95%;
 	}
@@ -146,7 +163,7 @@ function wordpress_svg_icons_cheat_sheet(){
 	::selection { 
 		background: #FF8000;
 	}	
-		
+	
 	.how-to-use { width:749px;  border: 1px solid #CCCCCC; margin-bottom: 5px; float:left;}
 	.tips-box { width: 500px; height: 236px; float: left; margin-left: 25px; border: 1px solid #cccccc;}
 	.help-boxes { width: 100%; height: 236px; }
@@ -156,7 +173,7 @@ function wordpress_svg_icons_cheat_sheet(){
 	<div class="w-main centered">
 	
 	<header>
-		<h1>The <i style="color:#FF8000;">WordPress SVG Icons</i> plugin contains the following icons</h1>
+		<h1>The <i style="color:#FF8000;">WP SVG Icon Set #1</i> Plugin contains the following icons</h1>
 		<h4>These icons are scaleable vector graphics, meaning you can set them to whatever size you want with out any loss in quality. <span style="color:#FF8000;">Enjoy!</span></h3>
 	</header>
 	<div class="help-boxes" >
@@ -166,14 +183,14 @@ function wordpress_svg_icons_cheat_sheet(){
 			<ul style="margin-left:35px; list-style-type:square; margin-bottom: 20px;">
 				<li>Step 1: Locate and click the icon you want to use.</li>
 				<li>Step 2: Copy the code out of the example box</li>
-				<li style="padding-left:50px; list-style-type:none !important; display:inline;"><i style="color:red;">example:</i> <input class="copy_paste_input" style='width:298px;' readonly type='text' value='<div data-icon="Unicode Here" ></div>'></li>
+				<li style="padding-left:50px; list-style-type:none !important; display:inline;"><i style="color:red;">example:</i> <input class="copy_paste_input" style='width:298px;' readonly type='text' value='<div data-icon="Unicode Here"></div>'></li>
 				<li>Step 3: Insert the code anywhere you want your icon to appear on a page.</li>
 			</ul>
 			<div class="wp-svg-icon-preview-box"><i style="font-size:14px;" class="copy-paste-text">Icon Preview:</i><b class="wp-svg-icon-preview"></b></div>
 		</div>	<!-- end how to use -->
 		<div class="tips-box">
 			<h3 style="padding-left:10px;">Tips:</h3>
-			<div class="wp-svg-paypal-donation-button" style="position:inherit; display:inline; float:left; margin-left:200px;">
+			<div class="wp-svg-paypal-donation-button" style="position:inherit; width:500px; display:inline; float:left; margin-left:200px;">
 			<b style="text-decoration:underline; float:left; margin-top:5px; font-size:13px;">Buy me a Monster Energy Drink</b>
 			<form action="https://www.paypal.com/cgi-bin/webscr" style=" float:left;" method="post" target="_top">
 				<input type="hidden" name="cmd" value="_donations">
@@ -2201,28 +2218,21 @@ function wordpress_svg_icons_cheat_sheet(){
 		<a class="glyph-link" href="#"><input class="glyph_unicode" type="text" readonly="readonly" value="&amp;#xe1f3;" >  </a>
 	</div>
 	</section>
-	<span style="display:inline-block"><img style="width:15%; margin-right:.5em; float:left;" src="<?php echo plugins_url( '/svg-vector-icon-plugin/includes/images/New-Logo.png' ); ?>" /><h3 style="float:left;"> Expansion Pack </h3></span>
-	<p>Welcome to the new expansion pack section! This section will grow as time progresses. But in the meantime, here's a rounded linked in icon for you!</p>
-	
-	<p>Unfortunately since the release of this plugin, the original source of these web font icons began releasing these without proper unicode support.So as of right now, 
-		the icons in the expansion pack must be inlcuded by adding a class to an element. Clicking the icon will still provide you with the necessary code
-		to display the icon, it just will no longer be in Unicode form. I'm terribly sorry for the inconvinence.</p> 
-
-	<p style="color:#FF8000;">If you need an icon (or icons) included for a project immediately, please make a donation and I will include them as soon as possible.</p>
-		
-	<p style="color:#E27F7F;">If you have designed icons you would like to include in this plugin, I would be delighted to include them and give you attribution. Just get in contact with me <a href="http://www.evan-herman.com/contact/" target="_blank">here</a>.</p>
-	
-	<div class="expansion-Glyph">
-		<span class="fs1 wp-svg-icon-set1-expansion-linkedin_circle"></span>
-		<a class="glyph-link" href="#"><input class="expansion_glyph_unicode" type="text" readonly="readonly" value="wp-svg-icon-set1-expansion-linkedin_circle" >  </a>
-	</div>
 	</div>
 	<div class="clear"></div>
+	
 	<footer>
 		<p>Plugin Created By <a style="color:#B35047;" href="http://www.Evan-Herman.com" target="_blank">Evan Herman</a></p>
 	</footer>
 	</div>
-	
+	<script>
+	document.getElementById("glyphs").addEventListener("click", function(e) {
+		var target = e.target;
+		if (target.tagName === "INPUT") {
+			target.select();
+		}
+	});
+	</script>
 </body>
 </html>
 
